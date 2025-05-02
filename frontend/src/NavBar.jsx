71e1720a17e4633d99c36b9def29f4212fe4c5e3
Moshe Swed <mswed@beapot.com>
2025-05-02 18:21:19 -0400

    conditional navbar

--- frontend/src/NavBar.jsx
+++ frontend/src/NavBar.jsx
@@ -1,7 +1,10 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar, Nav, Container } from 'react-bootstrap';
+import { AuthContext } from './AuthProvider';
 
 const NavBar = () => {
+  const { token } = useContext(AuthContext);
   return (
     <Navbar bg="light" variant="light" expand="lg" className="w-100">
       <div className="container-fluid">
@@ -11,21 +14,28 @@ const NavBar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link as={Link} to={'/companies'}>
-              Companies
-            </Nav.Link>
-            <Nav.Link as={Link} to={'/jobs'}>
-              Jobs
-            </Nav.Link>
-            <Nav.Link as={Link} to={'/login'}>
-              Login
-            </Nav.Link>
-            <Nav.Link as={Link} to={'/signup'}>
-              Signup
-            </Nav.Link>
-            <Nav.Link as={Link} to={'/profile'}>
-              Profile
-            </Nav.Link>
+            {token ? (
+              <>
+                <Nav.Link as={Link} to={'/companies'}>
+                  Companies
+                </Nav.Link>
+                <Nav.Link as={Link} to={'/jobs'}>
+                  Jobs
+                </Nav.Link>
+                <Nav.Link as={Link} to={'/profile'}>
+                  Profile
+                </Nav.Link>
+              </>
+            ) : (
+              <>
+                <Nav.Link as={Link} to={'/login'}>
+                  Login
+                </Nav.Link>
+                <Nav.Link as={Link} to={'/signup'}>
+                  Signup
+                </Nav.Link>
+              </>
+            )}
           </Nav>
         </Navbar.Collapse>
       </div>
